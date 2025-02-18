1645fb80c4f44a541c84c5ce39d4c1c3d667ab34
= <=>
2025-02-18 17:01:08 +0800

    initial commit

--- Output3.py
+++ Output3.py
@@ -12,10 +12,18 @@ while quote != "q":
     try:
         val = {"quote": split_val[0], "author": split_val[1]}
         list.append(val)
+
+        count = 1
         for li in list:
-            print(li.get("quote"), " - ", li.get("author"))
-        res = random.randint(0, len(list) - 1)
-        print("random quote: ", list[res].get("quote"), " - ", list[res].get("author"))
+            print(count, ". ", li.get("quote"), " - ", li.get("author"))
+            count += 1
+
+        select = input("input the quote you want to display: ")
+        split = select.split(" ")
+
+        for sp in split:
+            print(list[int(sp)-1].get("quote"), " - ", list[int(sp)-1].get("author"))
+
     except:
         if quote == "q":
             print("Program terminated")
