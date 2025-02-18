a06650321b722161bf1277ef6dd45e14cc4334db
= <=>
2025-02-18 08:53:31 +0800

    initial commit

---
+++ Programming_Activity1.py
@@ -0,0 +1,7 @@
+
+a = input("input first number: ")
+b = input("input second number: ")
+
+sum = int(a) + int(b)
+
+print("the sum of ", a, " and ", b, " is ", sum)
---
+++ Programming_Activity2.py
@@ -0,0 +1,12 @@
+list = []
+
+n = int(input("enter list size: "))
+
+for i in range(n):
+
+    el = input(f"input something at index {i+1}: ")
+
+    list.append(el)
+
+print(list)
+
